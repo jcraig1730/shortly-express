9624e543dbc8040c081a27895aac93a36626eaa8
jcraig1730 <jcraig1730@gmail.com>
2019-08-10 16:23:51 -0700

    Added new session when no cookies on request

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,14 +2,32 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  req.session = {};
-  models.Sessions.create().then(session => {
-    models.Sessions.get({id: session.insertId}).then(data => {
+  if (!req.session) {
+    req.session = {};
+    req.session.user = {};
+  }
+  if (req.cookies) {
+    models.Sessions.get({hash: req.cookies.shortlyid}).then(results => {
+      req.session.user.username = results.user.username;
+      req.session.userId = results.userId;
+    }).catch(err => console.log(err))
+  }
+  console.log(req.session)
+  models.Sessions.create()
+    .then(session => {
+      return models.Sessions.get({id: session.insertId}
+    )
+    .then(data => {
       req.session.hash = data.hash;
-    }).then(() => {
-      next();
+      if (!res.cookies.shortlyid) {
+        res.cookies.shortlyid = {};
+        res.cookies.shortlyid.value = data.hash;
+      }
+      })
+      .then(() => {
+        next();
+      });
     });
-  });
 };
 
 /************************************************************/
